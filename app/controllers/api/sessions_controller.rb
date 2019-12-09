ea77ee12525aa262ed0daaf69fe62ac2479dcf57
Mannan Kasliwal <mkasliw1@binghamton.edu>
2019-12-09 14:52:36 -0500

    Finished initial model and controller files for user_auth (user and sessions), users can currently be created in the database and login/logout is working

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -6,7 +6,7 @@ class Api::SessionsController < ApplicationController
       login!(@user)
       render 'api/users/show'
     else
-      render json: ['Invalid credentials'], status: 401
+      render json: ['We could not find an account with those credentials. Please try again.'], status: 401
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,8 @@
 class User < ApplicationRecord
   attr_reader :password
 
-  validates :email, :password_digest, :session_token, presence: true
+  validates :email, :session_token, presence: true
+  validates :password_digest, presence: { message: 'Your password can\'t be blank.' }
   validates :email, :session_token, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true }
 
