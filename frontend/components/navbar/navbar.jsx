fbee38322cf0d4f4661c2ee80bad05383d922f7c
Mannan Kasliwal <mkasliw1@binghamton.edu>
2019-12-09 20:40:29 -0500

    Changed NavBar into class component to make it neater, withRouter() correctly passing props to NavBar (will be used to determine what links to show in the navbar based on current url path)

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -1,22 +1,52 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const NavBar = ({currentUser, logout}) => (
-  // if currentUser exists, render this
-  currentUser ?
-  (
-    // logout button will be replaced with a modal that has a logout link inside
-    <div className="nav-bar signed-in">
-      <p>Welcome, {currentUser.email}!</p>
-      <button onClick={logout}>Logout</button>
-    </div>
-  ) : 
-  //otherwise, render this
-  (
-    <div className="nav-bar signed-out">
-        <Link to="/login" className="login-link">Sign In</Link>
-    </div>
-  )
-);
+// const NavBar = ({currentUser, logout}) => (
+//   // if currentUser exists, render this
+//   currentUser ?
+//   (
+//     // logout button will be replaced with a modal that has a logout link inside
+//     <div className="nav-bar signed-in">
+//       <p>Welcome, {currentUser.email}!</p>
+//       <button onClick={logout}>Logout</button>
+//     </div>
+//   ) : 
+//   //otherwise, render this
+//   (
+//     <div className="nav-bar signed-out">
+//         <Link to="/login" className="login-link">Sign In</Link>
+//     </div>
+//   )
+// );
+
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { currentUser, logout } = this.props;
+
+    // use this.props.match.path here to decide if you want to show sign in on the page we're currently on
+
+    return (
+      // if currentUser exists, render this
+      currentUser ?
+        (
+          // logout button will be replaced with a modal that has a logout link inside
+          <div className="nav-bar signed-in">
+            <p>Welcome, {currentUser.email}!</p>
+            <button onClick={logout}>Logout</button>
+          </div>
+        ) :
+        //otherwise, render this
+        (
+          <div className="nav-bar signed-out">
+            <Link to="/login" className="login-link">Sign In</Link>
+          </div>
+        )
+    );
+  }
+}
 
 export default NavBar;
