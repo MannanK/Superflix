d190863e1af41967cd175a34efdef23bb32b4196
Mannan Kasliwal <mkasliw1@binghamton.edu>
2019-12-11 13:15:24 -0500

    Added a seeded demo user, heroku test 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+demoUser = User.create!(email: "peter@parker.com", password: "uncleben")
